3d03f9f36e019648e55caec79065ed0a6b7d8e8d
Ashvin Panicker <agnjkafgh@gmail.com>
2025-04-07 23:05:23 +0530

    Directives

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
@@ -25,5 +26,21 @@ export class TodosComponent {
     .subscribe((todos) => {
       this.todoItems.set(todos);
     });
+  }
+
+  updateTodoItem(todoItem: Todo) {
+    
+    this.todoItems.update((todos: Todo[]) => {
+      return todos.map(todo => {
+        if(todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        } 
+
+        return todo;
+      })
+    })
   }
 }
